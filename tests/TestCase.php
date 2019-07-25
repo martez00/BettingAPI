c609795f20937fbe3f3a845c1da8509eab9d81f6
martynas.sebestinas <martynas@infotrans.lt>
2019-07-25 18:44:45 +0300

    Tets first commit.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,21 @@
 <?php
-
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
+/**
+ * Class TestCase.
+ */
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    /**
+     * @return TestCase
+     */
+    public function startJsonRequest()
+    {
+        return $this->withHeaders([
+            'Accept'       => 'application/json',
+            'Content-Type' => 'application/json',
+        ]);
+    }
 }
